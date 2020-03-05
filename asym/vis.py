e4dcd0883dbd12cf7655ff185df7be94c38c54d5
Clemens Hug <clemens.hug@gmail.com>
2020-03-05 17:12:05 -0600

    Add sliders for cell image

--- asym/vis.py
+++ asym/vis.py
@@ -47,31 +47,33 @@ def get_data(m):
 
 marker_cols = list(input_data.select_dtypes(include=np.number).columns)
 
-stats = PreText(text='', width=500)
+stats = PreText(text='', width=200)
 marker_select = Select(value="CD25", options=marker_cols)
 
 marker_slider = RangeSlider(start=0, end=1, value=(0, 1))
+cell_slider = RangeSlider(start=0, end=1, value=(0, 1))
 
 # set up plots
 
 source = ColumnDataSource(data=input_data)
 image_source = ColumnDataSource(data=dict(image=[], dw=[], dh=[]))
 
-marker_mapper = linear_cmap(field_name="marker_val", palette=inferno(8)[:-1], low=0, high=500)
+marker_mapper = linear_cmap(field_name="marker_val", palette=inferno(8)[:-1], low=0, high=500, high_color=None)
 
 umap_figure = figure(plot_width=800, plot_height=500,
               tools='pan,wheel_zoom,lasso_select,box_select,reset')
-umap_figure.circle('u1', 'u2', size=2, source=source,
+umap_figure.circle('u1', 'u2', size=3, source=source,
             line_color=marker_mapper, color=marker_mapper,
             selection_color="orange", alpha=0.6, nonselection_alpha=0.4, selection_alpha=0.8
 )
 color_bar = ColorBar(color_mapper=marker_mapper['transform'], width=8, location=(0,0))
 umap_figure.add_layout(color_bar, 'right')
 
+cell_mapper = bokeh.models.mappers.LinearColorMapper(viridis(5), low=0, high=1000, high_color=None)
 cell_figure = figure(plot_width=350, plot_height=350,
               tools='pan,wheel_zoom,reset')
 cell_image = cell_figure.image(
-    image="image", color_mapper=bokeh.models.mappers.LinearColorMapper(viridis(5), low=0, high=1000, high_color=None),
+    image="image", color_mapper=cell_mapper,
     x=0, y=0, dw="dw", dh="dh", source=image_source
 )
 
@@ -81,6 +83,9 @@ def marker_change(attrname, old, new):
     # marker_select.options = nix(new, marker_cols)
     update()
 
+def cell_slider_change(attrname, old, new):
+    cell_mapper.low = new[0]
+    cell_mapper.high = new[1]
 
 def marker_slider_change(attrname, old, new):
     marker_mapper["transform"].low = new[0]
@@ -109,16 +114,21 @@ def selection_change(attrname, old, new):
         "dw": [cell_stack.shape[1]],
         "dh": [cell_stack.shape[2]],
     }
+    image_extr = mean_image.min(), mean_image.max()
+    cell_slider.start = image_extr[0]
+    cell_slider.end = image_extr[1]
+    cell_slider.value = (image_extr[0], image_extr[1])
     stats.text = "n cells: " + str(len(selected))
 
 
 source.selected.on_change('indices', selection_change)
 marker_select.on_change('value', marker_change)
 marker_slider.on_change("value", marker_slider_change)
+cell_slider.on_change("value", cell_slider_change)
 
 # set up layout
 widgets = column(marker_select, stats)
-main_row = row(column(umap_figure, marker_slider), widgets, cell_figure)
+main_row = row(column(umap_figure, marker_slider), widgets, column(cell_figure, cell_slider))
 layout = column(main_row)
 
 # initialize
