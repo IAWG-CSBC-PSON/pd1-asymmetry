8616c1be7f636062abb3158ec9a14b43d3c1f398
Clemens Hug <clemens.hug@gmail.com>
2020-03-22 18:05:15 -0400

    Add hover style. Move sliders to the side

--- asym/vis.py
+++ asym/vis.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 import bokeh
-from bokeh.palettes import viridis, inferno, Set3
+from bokeh.palettes import viridis, inferno, Colorblind
 from bokeh.io import curdoc
 from bokeh.layouts import column, row
 from bokeh.models import (
@@ -13,6 +13,7 @@ from bokeh.models import (
     Circle,
     ColorBar,
     ColumnDataSource,
+    HoverTool,
     PreText,
     RadioButtonGroup,
     RangeSlider,
@@ -25,10 +26,18 @@ from bokeh.transform import linear_cmap, factor_cmap
 CELL_IMAGE_METRICS = (["mean", "sd", "min", "max"], [np.mean, np.std, np.min, np.max])
 
 
+def round_signif(x, n=2):
+    return float(
+        np.format_float_positional(
+            x, precision=n, unique=False, fractional=False, trim="k"
+        )
+    )
+
+
 def prepare_server(doc, input_data, cell_stack, cell_markers=None):
     @lru_cache()
     def get_data(m=None):
-        d = input_data.copy()
+        d = input_data
         if m is not None:
             m_v = d[m]
             if np.issubdtype(m_v.dtype, np.number):
@@ -43,7 +52,7 @@ def prepare_server(doc, input_data, cell_stack, cell_markers=None):
 
     @lru_cache()
     def get_cat_colors(n):
-        return Set3[max(3, n)][:n]
+        return Colorblind[max(3, n)][:n]
 
     @lru_cache()
     def marker_cols(lower=False):
@@ -51,6 +60,8 @@ def prepare_server(doc, input_data, cell_stack, cell_markers=None):
         if lower:
             return {x.lower(): x for x in markers}
         return markers
+
+    input_data = input_data.copy()
 
     # Marker selection for UMAP plots
     ###########################################################################
@@ -63,7 +74,9 @@ def prepare_server(doc, input_data, cell_stack, cell_markers=None):
         min_characters=1,
         placeholder="Search for marker",
     )
-    marker_slider = RangeSlider(start=0, end=1, value=(0, 1), step=0.1)
+    marker_slider = RangeSlider(
+        start=0, end=1, value=(0, 1), step=0.1, orientation="vertical", direction="rtl"
+    )
 
     # Data sources
     ###########################################################################
@@ -81,6 +94,7 @@ def prepare_server(doc, input_data, cell_stack, cell_markers=None):
         active_scroll="wheel_zoom",
         active_drag="box_select",
         active_tap="tap",
+        toolbar_location="left",
     )
     marker_mapper = linear_cmap(
         field_name="marker_val_num",
@@ -89,22 +103,28 @@ def prepare_server(doc, input_data, cell_stack, cell_markers=None):
         high=500,
         high_color=None,
     )
-    umap_figure.circle(
+    umap_scatter_renderer = umap_figure.circle(
         "d1",
         "d2",
         size=8,
         source=source,
-        alpha=0.5,
+        fill_alpha=0.5,
+        line_alpha=0.9,
         fill_color=marker_mapper,
-        line_color=None,
-        selection_alpha=0.8,
+        line_color=marker_mapper,
+        selection_fill_alpha=0.8,
+        selection_line_alpha=1,
         selection_line_color="black",
         nonselection_alpha=0.2,
+        hover_line_color="black",
     )
     umap_color_bar = ColorBar(
         color_mapper=marker_mapper["transform"], width=12, location=(0, 0)
     )
     umap_figure.add_layout(umap_color_bar, "right")
+    umap_figure.add_tools(
+        HoverTool(tooltips=None, renderers=[umap_scatter_renderer], mode="mouse")
+    )
 
     # UMAP scatter plot for categorical data
     ###########################################################################
@@ -118,6 +138,7 @@ def prepare_server(doc, input_data, cell_stack, cell_markers=None):
         active_tap="tap",
         x_range=umap_figure.x_range,
         y_range=umap_figure.y_range,
+        toolbar_location="left",
     )
     marker_mapper_cat = factor_cmap(
         field_name="marker_val_cat", palette=["#000000"], factors=["a"]
@@ -128,14 +149,14 @@ def prepare_server(doc, input_data, cell_stack, cell_markers=None):
         size=8,
         source=source,
         legend_field="marker_val_cat",
-        alpha=0.5,
+        alpha=0.7,
         fill_color=marker_mapper_cat,
         line_color=None,
-        selection_alpha=0.8,
+        selection_alpha=0.9,
         selection_line_color="black",
-        nonselection_alpha=0.2,
+        nonselection_alpha=0.5,
     )
-    umap_figure_cat.legend.location = "center_right"
+    umap_figure_cat.legend.location = "top_right"
     umap_figure_cat.legend.orientation = "vertical"
 
     # Cell picture plot
@@ -156,7 +177,9 @@ def prepare_server(doc, input_data, cell_stack, cell_markers=None):
     cell_markers_select = Select(
         value=cell_markers[0][0], options=cell_markers, title="Marker cell image"
     )
-    cell_slider = RangeSlider(start=0, end=1, value=(0, 1))
+    cell_slider = RangeSlider(
+        start=0, end=1, value=(0, 1), orientation="vertical", direction="rtl"
+    )
     metric_select = RadioButtonGroup(active=0, labels=CELL_IMAGE_METRICS[0])
     stats = PreText(text="", width=100)
 
@@ -164,7 +187,12 @@ def prepare_server(doc, input_data, cell_stack, cell_markers=None):
         viridis(20), low=0, high=1000, high_color=None
     )
     cell_color_bar = ColorBar(color_mapper=cell_mapper, width=12, location=(0, 0))
-    cell_figure = figure(plot_width=450, plot_height=350, tools="pan,wheel_zoom,reset")
+    cell_figure = figure(
+        plot_width=450,
+        plot_height=350,
+        tools="pan,wheel_zoom,reset",
+        toolbar_location="left",
+    )
     cell_image = cell_figure.image(
         image="image",
         color_mapper=cell_mapper,
@@ -210,11 +238,14 @@ def prepare_server(doc, input_data, cell_stack, cell_markers=None):
             marker_mapper_cat["transform"].palette = get_cat_colors(len(levels))
             marker_mapper_cat["transform"].factors = levels
         elif update_range and numeric_marker:
-            marker_max = d["marker_val_num"].max()
-            marker_min = d["marker_val_num"].min()
+            marker_max = round_signif(d["marker_val_num"].max())
+            marker_min = round_signif(d["marker_val_num"].min())
             marker_slider.start = marker_min
             marker_slider.end = marker_max
-            marker_slider.value = tuple(np.percentile(d["marker_val_num"], [5, 95]))
+            marker_slider.step = round_signif((marker_max - marker_min) / 50)
+            marker_slider.value = tuple(
+                map(round_signif, np.percentile(d["marker_val_num"], [5, 95]))
+            )
         umap_figure.visible = numeric_marker
         umap_figure_cat.visible = not numeric_marker
         marker_slider.visible = numeric_marker
@@ -234,9 +265,10 @@ def prepare_server(doc, input_data, cell_stack, cell_markers=None):
             "dw": [cell_stack.shape[1]],
             "dh": [cell_stack.shape[2]],
         }
-        image_extr = mean_image.min(), mean_image.max()
+        image_extr = round_signif(mean_image.min()), round_signif(mean_image.max())
         cell_slider.start = image_extr[0]
         cell_slider.end = image_extr[1]
+        cell_slider.step = round_signif((image_extr[1] - image_extr[0]) / 50)
         cell_slider.value = image_extr
         stats.text = "n cells: " + str(len(selected))
 
@@ -283,13 +315,14 @@ def prepare_server(doc, input_data, cell_stack, cell_markers=None):
         column(
             marker_select,
             marker_input,
-            umap_figure,
+            row(umap_figure, marker_slider),
             umap_figure_cat,
-            marker_slider,
             row(edit_selection_col, edit_selection_val, edit_selection_submit),
             edit_selecton_log,
         ),
-        column(cell_markers_select, metric_select, cell_figure, stats, cell_slider),
+        column(
+            cell_markers_select, metric_select, row(cell_figure, cell_slider), stats
+        ),
     )
 
     # initialize
